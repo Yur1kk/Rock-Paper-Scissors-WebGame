0cc7518589cee8f38a3a01fe30f8655ef6442783
Yurii Soroka <urasor04@gmail.com>
2024-08-02 15:39:01 +0300

    Main file with the game logic, added logic of player choice, providing functionality of game start

---
+++ app.js
@@ -0,0 +1,25 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+const getPlayerChoice = () => {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    if (selection !== ROCK &&
+         selection !== PAPER &&
+          selection !== SCISSORS) {
+            alert(`Invalid choice input! We chose ${DEFAULT_USER_CHOICE} for you`);
+            return DEFAULT_USER_CHOICE;
+          }
+          return selection;
+}
+
+const startGame = () => {
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+}
+
+startGameBtn.addEventListener('click', startGame);
