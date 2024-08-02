9db39a69a51decf6b288ac62809463a1324ae3f0
Yurii Soroka <urasor04@gmail.com>
2024-08-02 16:16:03 +0300

    Added logic for computer choice and logic to calculate the winner of the game

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -18,6 +21,29 @@ const getPlayerChoice = () => {
           return selection;
 }
 
+const getComputerChoice = () => {
+  const randomChoice = Math.random();
+  if (randomChoice < 0.34) {
+    return ROCK;
+  } else if (randomChoice < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+}
+
+const getWinner = (cChoice, pChoice) => {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (cChoice === ROCK && pChoice === PAPER
+     || cChoice === PAPER && pChoice === SCISSORS || 
+    cChoice === SCISSORS && pChoice === ROCK) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+}
+
 const startGame = () => {
   if (gameIsRunning) {
     return;
@@ -25,7 +51,9 @@ const startGame = () => {
   gameIsRunning = true;
     console.log('Game is starting...');
     const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice, playerSelection);
+    console.log(winner);
 }
 
 startGameBtn.addEventListener('click', startGame);
